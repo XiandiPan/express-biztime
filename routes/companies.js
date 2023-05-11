183d927c9499884d7c9a5a80adbbbc248ef5ed24
Michael <mherman315@gmail.com>
2023-05-11 12:35:41 -0700

    Added Insomnia file, finished POST. Write PUT

--- routes/companies.js
+++ routes/companies.js
@@ -5,32 +5,55 @@ const { BadRequestError } = require("../expressError");
 const router = express.Router();
 
 
-router.get("/", async function(req, res, next){
+/** Get all companies */
+router.get("/", async function (req, res, next) {
   const results = await db.query(
     `SELECT code, name
       FROM  companies`
   );
-  console.log("result",results);
+  // console.log("result",results);
 
   const companies = results.rows;
 
-  console.log("rows",companies);
+  // console.log("rows", companies);
 
   return res.json({ companies });
-})
+});
 
 
+/** GET company by code */
+router.get("/:code", async function (req, res, next) {
+  const code = req.params.code;
+
+  const results = await db.query(
+    `SELECT code, name, description
+    FROM companies
+    WHERE code = $1`, [code]);
+
+  const company = results.rows;
+  if (company.length === 0) {
+    throw new BadRequestError;
+  }
+  // console.log("Company =====", company);
+  return res.json({ company });
+});
 
 
+/** Adds new company to DB */
+router.post("/", async function (req, res, next) {
+  if (!req.body) throw new BadRequestError();
 
+  const { code, name, description } = req.body;
+  const result = await db.query(
+    `INSERT INTO companies (code, name, description)
+           VALUES ($1, $2, $3)
+           RETURNING code, name, description`,
+    [code, name, description],
+  );
 
-
-
-
-
-
-
-
+  const company = result.rows[0];
+  return res.status(201).json({ company });
+});
 
 
 
